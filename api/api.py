eda14fa977d396485c6a9f45dd52a6bf8d4db090
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-05-14 02:19:25 +0100

    API With Read and Write

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 from datetime import datetime
 from opcua import Client, ua
 from typing import Optional
@@ -26,8 +27,7 @@ def ReadAllOPCUAValues(opcUrl : str, UserName : Optional[str] = None, Password :
         security_settings.set_user_token_policy(user_token_policy)
         client.set_security_string(security_settings)
         client = Client(opcUrl)
-        
-    
+
     client.connect()
     CurrentNodeValues = {}
     root = client.get_root_node()
@@ -42,6 +42,24 @@ def ReadAllOPCUAValues(opcUrl : str, UserName : Optional[str] = None, Password :
     
     client.disconnect()
     return CurrentNodeValues
+
+def WriteOPCUAValues(opcUrl : str, nodeid : str, value : str, UserName : Optional[str] = None, Password : Optional[str] = None , Secure_Policy : Optional[str] = None):
+    if (UserName is None) and (Password is None) and (Secure_Policy is None) :
+        client = Client(opcUrl)
+    else:
+        security_policy = security_policies[Secure_Policy]
+        user_token_policy = ua.UserTokenPolicy()
+        user_token_policy.set_userpass(UserName, Password)
+        security_settings = ua.SecuritySettings()
+        security_settings.set_security_policy(security_policy)
+        security_settings.set_user_token_policy(user_token_policy)
+        client.set_security_string(security_settings)
+        client = Client(opcUrl)
+        
+    client.connect()
+    client.get_node(nodeid).set_value(value)
+
+    client.disconnect()
 
 app = FastAPI()
 
@@ -60,3 +78,9 @@ def opcuavalues(opcUrl : str, UserName : Optional[str] = None, Password : Option
 def opcuavalues(opcUrl : str, nodeid : str, UserName :  Optional[str] = None, Password :  Optional[str] = None , Secure_Policy :  Optional[str] = None):
     NodeColletion = ReadAllOPCUAValues(opcUrl, UserName, Password, Secure_Policy)
     return {node_id: Current_value for node_id, Current_value in NodeColletion.items() if nodeid in node_id}
+
+# Get Filtered Values of the OPC UA Server
+@app.put("/WriteOPCUAValues")
+def opcuavalues(opcUrl : str, nodeid : str, value : str, UserName :  Optional[str] = None, Password :  Optional[str] = None , Secure_Policy :  Optional[str] = None):
+    WriteOPCUAValues(opcUrl, nodeid, value, UserName, Password, Secure_Policy)
+    return {True}
