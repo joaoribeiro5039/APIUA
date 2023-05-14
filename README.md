8f6a8a155e3a18d1536d8a8595e25196bffd7d2c
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-05-14 10:45:27 +0100

    Dockerfile working for OPC UA Server

--- README.md
+++ README.md
@@ -6,14 +6,13 @@ Solution for Data Sincronization between Several OPC UA Servers
 sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
+pip install --upgrade pip
 
 # Python API
-pip install --upgrade pip
 pip install "uvicorn[standard]"
 pip install "fastapi[all]"
 
 # Python OPC UA Server
-pip install --upgrade pip
 pip install opcua
 
 
--- SimpleOPCUAServer/opcuaserver.py
+++ SimpleOPCUAServer/opcuaserver.py
@@ -1,20 +1,99 @@
 from opcua import ua, Server
-import json
 import time
 import random
 import datetime
+import json
+
+opcConfig = [
+    {
+        "node_id": "ns=1;s=Machine1",
+        "name": "Machine1",
+        "variables": [
+            {
+                "node_id": "ns=1;s=Machine1.Speed",
+                "name": "Speed",
+                "value": 0,
+                "isRandom": True,
+                "range_min": 200,
+                "range_max": 400
+            },
+            {
+                "node_id": "ns=1;s=Machine1.Temperature",
+                "name": "Temperature",
+                "value": 0,
+                "range": 0,
+                "isRandom": True,
+                "range_min": 300,
+                "range_max": 400
+            },
+            {
+                "node_id": "ns=1;s=Machine1.State",
+                "name": "State",
+                "value": 0,
+                "isRandom": False,
+                "range_min": 0,
+                "range_max": 5
+            },
+            {
+                "node_id": "ns=1;s=Machine1.DateTime",
+                "name": "DateTime",
+                "value": 0,
+                "isRandom": False,
+                "range_min": 0,
+                "range_max": 0
+            }
+        ]
+    },
+    {
+        "node_id": "ns=1;s=Machine2",
+        "name": "Machine2",
+        "variables": [
+            {
+                "node_id": "ns=1;s=Machine2.Speed",
+                "name": "Speed",
+                "value": 0,
+                "isRandom": True,
+                "range_min": 200,
+                "range_max": 400
+            },
+            {
+                "node_id": "ns=1;s=Machine2.Temperature",
+                "name": "Temperature",
+                "value": 0,
+                "isRandom": True,
+                "range_min": 300,
+                "range_max": 400
+            },
+            {
+                "node_id": "ns=1;s=Machine2.State",
+                "name": "State",
+                "value": 0,
+                "isRandom": False,
+                "range_min": 0,
+                "range_max": 5
+            },
+            {
+                "node_id": "ns=1;s=Machine2.DateTime",
+                "name": "DateTime",
+                "value": 0,
+                "isRandom": False,
+                "range_min": 0,
+                "range_max": 0
+            }
+        ]
+    }
+]
 
 # create server
 server = Server()
 server.name = "SimpleOPCUA"
-server.set_endpoint("opc.tcp://192.168.1.242:4840")
+server.set_endpoint("opc.tcp://0.0.0.0:4840")
 
 # create objects and variables from json file
 with open("nodes.json", "r") as f:
-    nodes = json.load(f)
-
-
-for node in nodes:
+    jsonnodes = json.load(f)
+    
+for node in jsonnodes:
     obj = server.nodes.objects.add_object(node["node_id"], node["name"])
     for var in node["variables"]:
         opc_var = obj.add_variable(var["node_id"], var["name"], var["value"])
@@ -26,7 +105,7 @@ server.start()
 try:
     while True:
         time.sleep(0.01)
-        for node in nodes:
+        for node in opcConfig:
             for var in node["variables"]:
                 var_node = server.get_node(var["node_id"])
                 if var["isRandom"]:
@@ -38,3 +117,6 @@ try:
 
 finally:
     server.stop()
+
+
+
