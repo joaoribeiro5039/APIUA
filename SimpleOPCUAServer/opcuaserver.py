809ad51230e1380a3418f21c940d2fa2180c9726
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-05-14 01:28:18 +0100

    Setup Write Variables on OPC UA Server

--- SimpleOPCUAServer/opcuaserver.py
+++ SimpleOPCUAServer/opcuaserver.py
@@ -2,30 +2,38 @@ from opcua import ua, Server
 import json
 import time
 import random
+import datetime
 
 # create server
 server = Server()
-server.set_endpoint("opc.tcp://127.0.0.1:4840")
+server.name = "SimpleOPCUA"
+server.set_endpoint("opc.tcp://192.168.1.242:4840")
 
 # create objects and variables from json file
 with open("nodes.json", "r") as f:
     nodes = json.load(f)
 
+
 for node in nodes:
     obj = server.nodes.objects.add_object(node["node_id"], node["name"])
     for var in node["variables"]:
         obj.add_variable(var["node_id"], var["name"], var["value"])
+        obj.set_writable(True)
+        obj.set_access_level(ua.AccessLevel.CurrentReadOrWrite)
+        obj.set_user_access_level(ua.AccessLevel.CurrentReadAndWrite)
 
 # start server
 server.start()
 
 try:
     while True:
-        time.sleep(0.5)
+        time.sleep(0.01)
         for node in nodes:
             for var in node["variables"]:
-                value = random.randint(var["range_min"], var["range_max"])
-                var_node = server.get_node(var["node_id"])
-                var_node.set_value(value)
+                if var["isRandom"]:
+                    value = random.randint(var["range_min"], var["range_max"])
+                    var_node = server.get_node(var["node_id"])
+                    var_node.set_value(value)
+
 finally:
     server.stop()
