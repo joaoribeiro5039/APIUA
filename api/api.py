1459ff3dd16268709cccf4e959f367ea6e3c6d26
joaoribeiro5039 <joaoribeiro.5039@gmail.com>
2023-05-14 03:20:23 +0100

    Updated API for monitoring actibity

--- api/api.py
+++ api/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import asyncio
 from datetime import datetime
 from opcua import Client, ua
 from typing import Optional
@@ -61,6 +61,16 @@ def WriteOPCUAValues(opcUrl : str, nodeid : str, value : str, UserName : Optiona
 
     client.disconnect()
 
+async def Monitor(Freq : int, opcUrl : str, nodeid : str, UserName :  Optional[str] = None, Password :  Optional[str] = None , Secure_Policy :  Optional[str] = None): 
+    global StopFlag
+    while not StopFlag:
+        print("Starting task...")
+        NodeColletion = ReadAllOPCUAValues(opcUrl, UserName, Password, Secure_Policy)
+        MonitorCollection = {node_id: Current_value for node_id, Current_value in NodeColletion.items() if nodeid in node_id}
+        print(MonitorCollection)
+        await asyncio.sleep(1/Freq)
+        print("Task completed.")
+
 app = FastAPI()
 
 #Testing
@@ -84,3 +94,18 @@ def opcuavalues(opcUrl : str, nodeid : str, UserName :  Optional[str] = None, Pa
 def opcuavalues(opcUrl : str, nodeid : str, value : str, UserName :  Optional[str] = None, Password :  Optional[str] = None , Secure_Policy :  Optional[str] = None):
     WriteOPCUAValues(opcUrl, nodeid, value, UserName, Password, Secure_Policy)
     return {True}
+
+#Start Monitoring Activity
+@app.post("/StartMonitor")
+async def Start_Monitor(Freq : int, opcUrl : str, nodeid : str, UserName :  Optional[str] = None, Password :  Optional[str] = None , Secure_Policy :  Optional[str] = None):
+    global RunningTasks
+    global StopFlag
+    StopFlag = False
+    RunningTasks = asyncio.create_task(Monitor(Freq, opcUrl, nodeid, UserName, Password, Secure_Policy))
+
+    
+#Stop All Monitoring Activity
+@app.post("/StopMonitor")
+async def Start_Monitor():
+    global StopFlag
+    StopFlag = True
